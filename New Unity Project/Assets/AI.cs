38c056d265a38c54073e45887d4fecb95286384c
lkk194 <lkk194@naver.com>
2017-03-02 22:19:22 +0900

    update new project

---
+++ New Unity Project/Assets/AI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+    GameObject player;
+    //NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("player");
+        //nav = GetComponent<NavMeshAgent>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //nav.SetDestination(player.transform.position);
+	}
+}
